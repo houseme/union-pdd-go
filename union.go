79a8e0bda2421eb395e55cde202bb2b2d7ac20f7
houseme <housemecn@gmail.com>
2022-10-18 23:05:45 +0800

    feat: create UnionPDD

--- union.go
+++ union.go
@@ -5,3 +5,22 @@
 // You can obtain one at https://github.com/houseme/union-pdd-go.
 
 package pdd
+
+const (
+	version = "0.01"
+)
+
+// UnionPDD is a union service.
+type UnionPDD struct {
+	// contains filtered or unexported fields
+}
+
+// NewUnionPDD returns a union service.
+func NewUnionPDD() *UnionPDD {
+	return &UnionPDD{}
+}
+
+// Version returns the version of the union service.
+func (u *UnionPDD) Version() string {
+	return version
+}
